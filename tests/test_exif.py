38feb89a63e09fa3b9ec9f29639560039da30ea9
Stefan Bestle <stefan.bestle@gmail.com>
2026-06-06 22:12:24 +0200

    fix: read exif dates from heif files

--- tests/test_exif.py
+++ tests/test_exif.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from PIL import Image
+
 from utils import ExifUtils
 
 
@@ -23,6 +25,19 @@ def test_get_date_taken_no_exif(sample_jpg_file):
     """Test getting date taken from a file without EXIF data."""
     date_taken = ExifUtils.get_date_taken(str(sample_jpg_file))
     assert date_taken is None
+
+
+def test_get_date_taken_heif_file(temp_dir):
+    """Test getting date taken from EXIF data in a HEIF file."""
+    image_path = temp_dir / "test_image.hif"
+    image = Image.new("RGB", (100, 100), color="green")
+    exif_data = Image.Exif()
+    exif_data[ExifUtils.EXIF_DATETIME_ORIGINAL] = "2023:06:01 10:20:30"
+
+    image.save(image_path, format="HEIF", exif=exif_data)
+
+    date_taken = ExifUtils.get_date_taken(str(image_path))
+    assert date_taken == datetime(2023, 6, 1, 10, 20, 30)
 
 
 def test_get_date_taken_invalid_file(temp_dir):
